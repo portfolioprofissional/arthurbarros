e8d25703ba90efb1c5ce18abfeb2a21461d96c59
Arthur Barros <arthur@example.com>
2026-04-26 12:10:28 -0300

    Initial commit

---
+++ Estudo de Java/estudo de java.java
@@ -0,0 +1,10 @@
+java: create java project
+package estudo.de.java;
+let nome= Davi Oliveira da Silva Ayoub;
+let idade= 25;
+let altura= 1.80;
+let peso= 97.5;
+console.log("Meu nome é " + nome);
+console.log("Minha idade é " + idade);
+console.log("Minha altura é " + altura);
+console.log("Meu peso é " + peso);
